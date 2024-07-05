260204f7f0979a173034e1d337e486205935862a
Esteban Arias <tapitarias@gmail.com>
2024-07-05 12:22:46 -0500

    Change docker image to ubuntu:24.04

---
+++ src/config/Args.ts
@@ -0,0 +1,10 @@
+export class ARGS {
+  public static META_LONG_TOKEN = process.env.META_LONG_TOKEN ?? "";
+  public static META_APP_ID = process.env.META_APP_ID ?? "";
+  public static MONGO_USER = process.env.MONGO_USER ?? "";
+  public static MONGO_PASSWORD = process.env.MONGO_PASSWORD ?? "";
+  public static MONGO_DATABASE = process.env.MONGO_DATABASE ?? "";
+  public static APP_ENV = process.env.APP_ENV ?? "";
+  public static META_USERNAME = process.env.META_USERNAME ?? "";
+  public static META_PASSWORD = process.env.META_PASSWORD ?? "";
+}
