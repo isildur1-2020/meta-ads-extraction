69f6ce5ed0d67da6c3690efe86b14bc95f95843e
Esteban Arias <tapitarias@gmail.com>
2024-07-05 15:53:04 -0500

    change scrapper dates

--- src/config/metaScrapper.ts
+++ src/config/metaScrapper.ts
@@ -1,7 +1,7 @@
 import { AD_COUNTRIES } from "../lib/constants";
 export const metaExtractorConfig = {
   search_terms: "",
-  ad_delivery_date_min: "2024-07-04",
-  ad_delivery_date_max: "2024-07-04",
+  ad_delivery_date_min: "2024-07-02",
+  ad_delivery_date_max: "2024-07-05",
   ad_reached_countries: [AD_COUNTRIES.ES],
 };
