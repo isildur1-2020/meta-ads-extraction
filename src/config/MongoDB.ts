114b7cc44f7e64324606732def409d396b743849
Esteban Arias <tapitarias@gmail.com>
2024-07-06 21:00:43 -0500

    Add MONGO_HOST on mongo config

--- src/config/MongoDB.ts
+++ src/config/MongoDB.ts
@@ -5,12 +5,28 @@ import { Logger } from "../lib/logs";
 export class MongoDB {
   public static async connect() {
     try {
-      const URL = `mongodb://${ARGS.MONGO_USER}:${ARGS.MONGO_PASSWORD}@mongodb-kiwanas:27017/${ARGS.MONGO_DATABASE}`;
+      this.checkVariables();
+      const URL = `mongodb://${ARGS.MONGO_USER}:${ARGS.MONGO_PASSWORD}@${ARGS.MONGO_HOST}:27017/${ARGS.MONGO_DATABASE}`;
       await mongoose.connect(URL);
       Logger.printProgressMsg("MongoDB Atlas connected successfully");
     } catch (err: any) {
       Logger.printErrMsg("[DATABASE] MongoDB Atlas error");
       throw new Error(err);
+    }
+  }
+
+  private static checkVariables() {
+    if (!ARGS.MONGO_USER) {
+      throw new Error("[DATABASE] MONGO_USER must be defined");
+    }
+    if (!ARGS.MONGO_PASSWORD) {
+      throw new Error("[DATABASE] MONGO_PASSWORD must be defined");
+    }
+    if (!ARGS.MONGO_HOST) {
+      throw new Error("[DATABASE] MONGO_HOST must be defined");
+    }
+    if (!ARGS.MONGO_DATABASE) {
+      throw new Error("[DATABASE] MONGO_HOST must be defined");
     }
   }
 }
